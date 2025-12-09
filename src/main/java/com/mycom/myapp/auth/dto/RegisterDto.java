10b53a00d4f3e0df23dc1ce3f2817bea3436d6bf
shin-0328 <113588811+shin-0328@users.noreply.github.com>
2025-12-09 17:23:09 +0900

    RegisterDto 생성
    RegisterDto 생성

---
+++ src/main/java/com/mycom/myapp/auth/dto/RegisterDto.java
@@ -0,0 +1,12 @@
+package com.mycom.myapp.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterDto {
+	private String username;
+	private String password;
+	private String nickname;
+}
